fc71f10d6cb6db2e0f98d506b4bea49f87d81ffd
pjs0719 <pjssoj07193@gmail.com>
2024-07-09 00:54:21 +0900

    반응형 테스트를 위한 기기 시뮬 패키지 설치

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';  // kReleaseMode 사용을 위해 추가
 import 'package:spaghetti/class_Service.dart';
 import 'package:spaghetti/startPage.dart';
 
 void main() {
   runApp(
-    MultiProvider(
+    DevicePreview(
+      enabled: !kReleaseMode,  // 릴리즈 모드가 아닌 경우에만 활성화
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => ClassService()),
         ChangeNotifierProvider(create: (_) => ClassOpinion()),
       ],
       child: MaterialApp(
+        builder: DevicePreview.appBuilder,  // DevicePreview.appBuilder 사용
+        useInheritedMediaQuery: true,       // MediaQuery 정보를 상속 받음
+        locale: DevicePreview.locale(context), // DevicePreview 로케일 사용
         home: PageSlideExample(),
         theme: ThemeData(fontFamily: 'NanumB'),
       ),
-    ),
-  );
+    );
+  }
 }
