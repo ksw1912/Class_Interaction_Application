dd2f8535d0dec8f5fe304b8bf666cf71085ede68
pbs <pbs1057@hs.ac.kr>
2024-07-11 17:31:45 +0900

    스타트 페이지 반응형으로 변경

--- lib/main/startPage.dart
+++ lib/main/startPage.dart
@@ -18,6 +18,9 @@ class PageSlideExampleState extends State<PageSlideExample> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenHeight = mediaQuery.size.height;
+    final screenWidth = mediaQuery.size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       body: PageView(
@@ -27,19 +30,24 @@ class PageSlideExampleState extends State<PageSlideExample> {
           Container(
             child: Stack(
               children: [
-                Align(
-                  alignment: Alignment(0.3, -0.1),
-                  child: Image.asset(
-                    'assets/images/1.png',
-                    height: 400,
+                Positioned(
+                  left: screenWidth * 0.09,
+                  top: screenHeight * 0.01,
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    height: screenHeight * 0.9,
+                    child: Image.asset('assets/images/1.png'),
                   ),
                 ),
-                Align(
-                  alignment: Alignment(0, 0.6),
-                  child: Text('자신의 의견을 반영하세요', style: TextStyle(fontSize: 20)),
+                Positioned(
+                  left: screenWidth * 0.25,
+                  top: screenHeight * 0.7,
+                  child: Text('자신의 의견을 반영하세요',
+                      style: TextStyle(fontSize: screenWidth * 0.05)),
                 ),
-                Align(
-                  alignment: Alignment(0, 0.8),
+                Positioned(
+                  left: screenWidth * 0.4,
+                  top: screenHeight * 0.775,
                   child: Image.asset('assets/images/Onboarding1.png',
                       width: 80, fit: BoxFit.fill),
                 ),
@@ -50,116 +58,119 @@ class PageSlideExampleState extends State<PageSlideExample> {
           Container(
             child: Stack(
               children: [
-                Align(
-                  alignment: Alignment(0, 0),
-                  child: Image.asset('assets/images/2.png'),
+                Positioned(
+                  left: screenWidth * 0.05,
+                  top: screenHeight * 0.01,
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    height: screenHeight * 0.9,
+                    child: Image.asset('assets/images/2.png'),
+                  ),
                 ),
-                Align(
-                  alignment: Alignment(0, 0.6),
-                  child:
-                      Text('부끄러울 땐 온라인으로 말해요', style: TextStyle(fontSize: 20)),
+                Positioned(
+                  left: screenWidth * 0.2,
+                  top: screenHeight * 0.7,
+                  child: Text('부끄러울 땐 온라인으로 말해요',
+                      style: TextStyle(fontSize: screenWidth * 0.05)),
                 ),
-                Align(
-                  alignment: Alignment(0, 0.8),
+                Positioned(
+                  left: screenWidth * 0.4,
+                  top: screenHeight * 0.775,
                   child: Image.asset('assets/images/Onboarding2.png',
                       width: 80, fit: BoxFit.fill),
                 ),
               ],
             ),
           ),
-          // 페이지 컨트롤러의 세 번째 위젯, 인덱스 : 2
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Image.asset(
-                      'assets/images/3.png', // 이미지 경로 설정
-                      height: 200,
-                      width: MediaQuery.of(context).size.width, // 화면 너비에 맞게 설정
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      '교수님과 학생이 직접 소통할 수 있어요',
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const LoginPage(role: "instructor"),
-                              ),
-                            );
-                          },
-                          child: Text('교수님으로 시작'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(255, 5, 179, 214),
-                            surfaceTintColor: Color.fromARGB(255, 5, 179, 214),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ), // 배경색 설정
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const LoginPage(role: "student"),
-                              ),
-                            );
-                          },
-                          child: Text('학생으로 시작'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color.fromARGB(192, 56, 255, 1),
-                            surfaceTintColor: Color.fromARGB(192, 56, 255, 1),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 100,
-                    ),
-                    SizedBox(
-                      height: 100,
-                    ),
-                    Image.asset('assets/images/Onboarding3.png',
-                        width: 100, fit: BoxFit.fill),
-                  ],
+
+          Container(
+            child: Stack(
+              children: [
+                Positioned(
+                  left: screenWidth * 0.1,
+                  top: screenHeight * 0.01,
+                  child: Container(
+                    width: screenWidth * 0.8,
+                    height: screenHeight * 0.95,
+                    child: Image.asset('assets/images/3.png'),
+                  ),
                 ),
-              ),
+                Positioned(
+                  left: screenWidth * 0.1,
+                  top: screenHeight * 0.7,
+                  child: Text('교수님과 학생이 직접 소통할 수 있어요',
+                      style: TextStyle(fontSize: screenWidth * 0.05)),
+                ),
+                Positioned(
+                  left: screenWidth * 0.1,
+                  top: screenHeight * 0.85,
+                  child: Container(
+                    width: screenWidth * 0.375,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(192, 5, 165, 0),
+                        surfaceTintColor: Color.fromARGB(192, 5, 165, 0),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const LoginPage(role: "instructor"),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "교수님으로 시작",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: screenWidth * 0.035),
+                      ),
+                    ),
+                  ),
+                ),
+                // 페이지 컨트롤러의 세 번째 위젯, 인덱스 : 2
+                Positioned(
+                  left: screenWidth * 0.525,
+                  top: screenHeight * 0.85,
+                  child: Container(
+                    width: screenWidth * 0.375, // 화면 너비의 80%
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 5, 179, 214),
+                        surfaceTintColor: Color.fromARGB(255, 5, 179, 214),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const LoginPage(role: "student"),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "학생으로 시작",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: screenWidth * 0.035),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: screenWidth * 0.4,
+                  top: screenHeight * 0.775,
+                  child: Image.asset('assets/images/Onboarding3.png',
+                      width: 80, fit: BoxFit.fill),
+                ),
+              ],
             ),
           ),
         ],
