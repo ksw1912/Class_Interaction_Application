c46479b672a31c3c8afef2955ce5365dc77c2d1f
KimSeoWon <ksw1912@gmail.com>
2024-07-22 01:54:18 +0900

    내파일apiURl

--- lib/ApiUrl.dart
+++ lib/ApiUrl.dart
@@ -1,13 +1,8 @@
 class Apiurl {
   //final String url =
   //    "https://0d3a6d00-d7ee-425d-8855-0bc4b4aaa1d0.mock.pstmn.io";
-<<<<<<< HEAD
-  // final String websocketUrl = "http://192.168.123.193:8080";
   final String url = "http://192.168.123.193:8080";
   final String websocketUrl = "192.168.123.193:8080";
-=======
-  final String url = "http://localhost:8082";
-  final String websocketUrl = "15.165.81.157:8082";
->>>>>>> dbbb10e5b20a65481cc7a26fc954f6ff5a4e5526
 }
-//15.165.81.157:8082
+//15.165.81.157:8082  지성님?
+//192.168.123.193 -> 서원
